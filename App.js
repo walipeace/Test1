81df1e0b63b76eb2492b9cdfc66c50a9dad8a7d8
walipeace <47972772+walipeace@users.noreply.github.com>
2019-02-27 16:19:42 +0800

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+      <Text>Wan Liana Atirah</Text>
+      <Text>1617360</Text>
       //Wan Liana Atirah binti Wan Faldli dan Nor Apiza
       //1617360
       </View>
